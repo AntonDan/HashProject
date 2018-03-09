54bc95a947b794a8a8d1f48b7ccf096479681b4d
AntonDan <anton.dan@hotmail.com>
2018-03-09 03:39:16 +0200

    Added structure for new solution

--- Program.cs
+++ Program.cs
@@ -79,8 +79,7 @@ namespace GoogleHashCode {
 			}
 		}
 
-		// 
-		static void Solution2(Data parser, Ride[] inputJobs) {
+		public static void Solution2(Data parser, Ride[] inputJobs) {
 			Array.Sort(inputJobs, (a, b) => Comparer<int>.Default.Compare(a.finish, b.finish)); // Sort jobs by finish time
 			List<Ride> rides = inputJobs.ToList();
 
@@ -92,42 +91,46 @@ namespace GoogleHashCode {
 			}
 			int index = 0, earliestAvailable = 0;
 			for (int step = 0; step < parser.TSteps; step = Math.Max(earliestAvailable, step + 1)) {
-				for (; index < rides.Count && rides[index].finish < step; ++index);
+				for (; index < rides.Count && rides[index].finish < step; ++index) ;
 				earliestAvailable = parser.TSteps;
 				for (int c = 0; c < cars.Length; ++c) {
+
 					Car car = cars[c];
+					// foreach(Choice choice in car.previousChoices) For every different previous decision
+					// List<Choice> newChoices = new List<Choice>(); List of top n scores
 					if (car.availableAt <= step) {
+						int count = 0;
 						int bestRide = -1;
 						float bestScore = System.Int32.MinValue;
 						int availableAt = 0;
 						int tempScore = 0;
+
 						for (int i = index; i < rides.Count; ++i) {
-							/*
-							foreach (var item in rides.Select((value, i) => new { i, value })) {
-							int i = item.i;
-							Ride ride = item.value;
-							*/
 							Ride ride = rides[i];
 							int startStep = Math.Max(step + GetDistance(car.posx, car.posy, ride.startx, ride.starty), ride.start);
 							int endStep = startStep + ride.duration;
 							if (endStep < ride.finish) {
-								int award = ride.duration + ((startStep <= ride.start) ? parser.Bonus : 0);
 								int waitTime = Math.Max(ride.start - (step + GetDistance(car.posx, car.posy, ride.startx, ride.starty)), 0);
 								/* Weights
-								 * nohurry    : 1 1 1 1
-								 * metropolis : 1 1.7 1 1
-								 * highbonus  : 1 1 1 1
+								 * nohurry    : 1 1 1 0.809 1
+								 * metropolis : 1 1 1.75 1 1
+								 * highbonus  : 1 1 1 1 1
 								 */
-								float score = award - waitTime - GetDistance(car.posx, car.posy, ride.startx, ride.starty) - (endStep  - step);
-								if (score > bestScore || (score >= bestScore && endStep < availableAt)) {
+								float score = ride.duration + ((startStep <= ride.start) ? parser.Bonus : 0) - waitTime * 1.75f - GetDistance(car.posx, car.posy, ride.startx, ride.starty) - (endStep - step);
+								// state worst = (newChoices.Count == maxSize) ? newChoices.Min() : null; 
+								if (score > bestScore || (score >= bestScore && endStep < availableAt)) { // if (worst == null || score > worst.score) if the list is not full or the score is better than the minimum
+																										  // if (score == null) newChoices.Add(new Choice(i, score, endStep, award))
+																										  // else worstChoice.Set(i, score, endStep, award); 
 									bestRide = i;
 									bestScore = score;
 									availableAt = endStep;
-									tempScore = award;
+									tempScore = ride.duration + ((startStep <= ride.start) ? parser.Bonus : 0);
 								}
 							}
 						}
-						if (bestRide != -1) {
+						// state best = (list.Count > 0) ? list.Max() : null; 
+						if (bestRide != -1) { // if (best.score > bestChoice.score) if the maximum score of the list is better than the max score of the 
+											  // bestChoice = best;
 							Ride ride = rides[bestRide];
 							car.ridesTaken.Add(ride);
 							car.posx = ride.endx;
@@ -137,9 +140,10 @@ namespace GoogleHashCode {
 							rides.RemoveAt(bestRide);
 						}
 					}
-					earliestAvailable = Math.Min(earliestAvailable, car.availableAt);
+					// end of loop
+					// find the depth 2 decision
+					earliestAvailable = Math.Min(earliestAvailable, car.availableAt); // preChoices
 				}
-
 			}
 			foreach (Car car in cars) {
 				Console.Write(car.ridesTaken.Count + " ");
@@ -150,9 +154,77 @@ namespace GoogleHashCode {
 			}
 			Console.WriteLine(totalScore);
 		}
+		
+		static void Solution3(Data parser, Ride[] inputJobs) {
+			Array.Sort(inputJobs, (a, b) => Comparer<int>.Default.Compare(a.finish, b.finish)); // Sort jobs by finish time
+			List<Ride> rides = inputJobs.ToList();
 
-		public static void Solution3() {
+			int totalScore = 0;
+
+			Car2[] cars = new Car2[parser.Fleet];
+			for (int i = 0; i < parser.Fleet; ++i) {
+				cars[i] = new Car2(i);
+			}
+			int index = 0, earliestAvailable = 0;
+			int maxSize = 5;
+			for (int step = 0; step < parser.TSteps; step = Math.Max(earliestAvailable, step + 1)) {
+				for (; index < rides.Count && rides[index].finish < step; ++index);
+				earliestAvailable = parser.TSteps;
+				for (int c = 0; c < cars.Length; ++c) {
+
+					Car2 car = cars[c];
+					for (int choiceIndex = 0; choiceIndex < car.choices.Count; ++choiceIndex) { // For every different previous decision (TODO: this should switch from foreach to for)
+						Choice choice = car.choices[choiceIndex];
+						if (choice.endStep <= step) {
+							List<Choice> newChoices = new List<Choice>(); // List of top n scores
+							for (int i = index; i < rides.Count; ++i) {
+								Ride ride = rides[i];
+								int startStep = Math.Max(step + GetDistance(choice.endx, choice.endy, ride.startx, ride.starty), ride.start);
+								int endStep = startStep + ride.duration;
+								if (endStep < ride.finish) {
+									int waitTime = Math.Max(ride.start - (step + GetDistance(choice.endx, choice.endy, ride.startx, ride.starty)), 0);
+									/* Weights
+									 * nohurry    : 1 1 1 0.809 1
+									 * metropolis : 1 1 1.75 1 1
+									 * highbonus  : 1 1 1 1 1
+									 */
+									float score = ride.duration + ((startStep <= ride.start) ? parser.Bonus : 0) - waitTime * 1.75f - GetDistance(choice.endx, choice.endy, ride.startx, ride.starty) - (endStep - step);
+									Choice worstChoice = (newChoices.Count == maxSize) ? newChoices.Min() : null;
+									if (worstChoice == null || score > worstChoice.score) {  // if the list is not full or the score is better than the minimum
+										int award = ride.duration + ((startStep <= ride.start) ? parser.Bonus : 0);
+										if (worstChoice == null) { // list is not full
+											List<Choice> temp = new List<Choice>(choice.prevChoices);
+											temp.Add(choice);
+											newChoices.Add(new Choice(i, choice.score + score, endStep, choice.award + award, ride.endx, ride.endy, temp)); // This might be bugged, temp should be all the previous choices that lent to the current choice but it needs to be disconnected from the rest choice lists
+										} else { // List is full so we just replace the minimum scoring choice with the current choice
+											worstChoice.Set(i, score, endStep, award, ride.endx, ride.endy);
+										}
+									}
+								}
+							}
+
+							if (newChoices.Count > 0) {		
+								/* TODO: If choice already has nextchoices (choice.nextChoices.Count == 0) then
+								 * Create a list with the maxSize highest scoring leaves of the 2 depth tree car.prevChoices (so the top scorers car.preChoices that have no children and the car.nextChoices of the car.preChoices that do have children) 
+								 * set that list as the new car.PrevChoices list
+								 * choiceIndex = 0
+								 */
+							}
+						}
+					}
+					// TODO: earliestAvailable = Math.Min(earliestAvailable, car.availableAt); // This is supposed to find the next (earliest) step in which a car will be available, with multiple choices car.availableAt is not a thing. If you're not going to implement this set earliestAvailable = step + 1
+				}
+			}
+			foreach (Car2 car in cars) {
+				/* TODO: Find the leaf of the tree with the highest award 
+				 * Get the previous choices of that leaf and print them  
+				 */
+
+			}
+			Console.WriteLine(totalScore);
 		}
+
+
 
 		static void Main(string[] args) {
 			Data parser = new Data();
@@ -161,8 +233,7 @@ namespace GoogleHashCode {
 			//parser.Rides[0].id = -1;
 			includedJobs = new List<Ride>();
 			Ride[] inputJobs = parser.Rides.ToArray();
-			Solution2(parser, inputJobs);
-
+			//Solution2(parser, inputJobs);
 		}
 	}
 }
